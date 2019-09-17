e1ac3bb040df695296cd82a87ad7768347100b6e
vincent-gao <vincent.gao@dpc.vic.gov.au>
2019-09-17 15:08:41 +1000

    SDPA-3129 Fixed jwt incompatible issue (#690)

--- src/Auth/AuthProvider.php
+++ src/Auth/AuthProvider.php
@@ -2,25 +2,13 @@
 
 namespace Drupal\tide_authenticated_content\Auth;
 
-use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\jwt\Authentication\Provider\JwtAuth;
-use Drupal\jwt\Transcoder\JwtTranscoderInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * JWT Authentication Provider.
  */
 class AuthProvider extends JwtAuth {
-
-  /**
-   * Constructs a HTTP basic authentication provider object.
-   *
-   * {@inheritdoc}
-   */
-  public function __construct(EntityTypeManager $manager, JwtTranscoderInterface $transcoder, EventDispatcherInterface $event_dispatcher) {
-    parent::__construct($transcoder, $event_dispatcher);
-  }
 
   /**
    * Override to use alternate headers (X-Authorization)
