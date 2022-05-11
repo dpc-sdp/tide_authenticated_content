38c7f0565facf700a98b2bbd7d863478d56e67ec
yeniatencio <yeni.atencio@dpc.vic.gov.au>
2022-05-12 09:16:32 +1000

    authenticated status

---
+++ src/Plugin/search_api/processor/AuthenticatedStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\tide_authenticated_content\Plugin\search_api\processor;
+
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\node\NodeInterface;
+use Entity;
+
+/**
+ * Excludes authenticated content from node indexes.
+ *
+ * @SearchApiProcessor(
+ *   id = "authenticated_status",
+ *   label = @Translation("Exclude authenticated status"),
+ *   description = @Translation("Remove authenticated content from being indexed."),
+ *   stages = {
+ *     "alter_items" = -1,
+ *   },
+ * )
+ */
+class AuthenticatedStatus extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterIndexedItems(array &$items) {
+    foreach ($items as $item_id => $item) {
+      $entity = $item->getOriginalObject()->getValue();
+      if ($entity instanceof NodeInterface && $entity->bundle() === 'landing_page') {
+        if (
+          $entity->hasField('field_authenticated_content')
+          && empty($entity->field_authenticated_content->getValue())
+        ) {
+          unset($items[$item_id]);
+        }
+      }
+    }
+  }
+}
